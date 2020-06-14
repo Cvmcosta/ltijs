a4c0284ea51e2062bd278dcb362fa02584fa2f5f
carlos <cvmcosta@yahoo.com.br>
2020-06-14 20:57:11 -0300

    feat(Deep Linking): Improved error handling. Same changes applied to names and roles

--- src/Provider/Services/DeepLinking.js
+++ src/Provider/Services/DeepLinking.js
@@ -32,7 +32,6 @@ class DeepLinking {
    */
   async createDeepLinkingForm (idtoken, contentItems, options) {
     const message = await this.createDeepLinkingMessage(idtoken, contentItems, options)
-    if (!message) return false
 
     // Creating auto submitting form
     const form = '<form id="ltijs_submit" style="display: none;" action="' + idtoken.platformContext.deepLinkingSettings.deep_link_return_url + '" method="POST">' +
@@ -56,71 +55,65 @@ class DeepLinking {
    */
   async createDeepLinkingMessage (idtoken, contentItems, options) {
     provDeepLinkingDebug('Starting deep linking process')
-    if (!idtoken) { provDeepLinkingDebug('IdToken object missing.'); return false }
-    if (!idtoken.platformContext.deepLinkingSettings) { provDeepLinkingDebug('DeepLinkingSettings object missing.'); return false }
-    if (!contentItems) { provDeepLinkingDebug('No content item passed.'); return false }
+    if (!idtoken) { provDeepLinkingDebug('IdToken object missing.'); throw new Error('MISSING_ID_TOKEN') }
+    if (!idtoken.platformContext.deepLinkingSettings) { provDeepLinkingDebug('DeepLinkingSettings object missing.'); throw new Error('MISSING_DEEP_LINK_SETTINGS') }
+    if (!contentItems) { provDeepLinkingDebug('No content item passed.'); throw new Error('MISSING_CONTENT_ITEMS') }
 
-    try {
-      // If it's not an array, turns it into an array
-      if (!Array.isArray(contentItems)) contentItems = [contentItems]
+    // If it's not an array, turns it into an array
+    if (!Array.isArray(contentItems)) contentItems = [contentItems]
 
-      // Gets platform
-      const platform = await this.#getPlatform(idtoken.iss, this.#ENCRYPTIONKEY, this.#logger, this.#Database)
-      if (!platform) {
-        provDeepLinkingDebug('Platform not found, returning false')
-        return false
-      }
-
-      provDeepLinkingDebug('Building basic JWT body')
-      // Builds basic jwt body
-      const jwtBody = {
-        iss: await platform.platformClientId(),
-        aud: idtoken.iss,
-        iat: Date.now() / 1000,
-        exp: Date.now() / 1000 + 60,
-        nonce: crypto.randomBytes(16).toString('base64'),
-        'https://purl.imsglobal.org/spec/lti/claim/deployment_id': idtoken.deploymentId,
-        'https://purl.imsglobal.org/spec/lti/claim/message_type': 'LtiDeepLinkingResponse',
-        'https://purl.imsglobal.org/spec/lti/claim/version': '1.3.0'
-      }
-
-      // Adding messaging options
-      if (options.message) jwtBody['https://purl.imsglobal.org/spec/lti-dl/claim/msg'] = options.message
-      if (options.errmessage) jwtBody['https://purl.imsglobal.org/spec/lti-dl/claim/errormsg '] = options.errmessage
-      if (options.log) jwtBody['https://purl.imsglobal.org/spec/lti-dl/claim/log'] = options.log
-      if (options.errlog) jwtBody['https://purl.imsglobal.org/spec/lti-dl/claim/errorlog'] = options.errlog
-
-      // Adding Data claim if it exists in initial request
-      if (idtoken.platformContext.deepLinkingSettings.data) jwtBody['https://purl.imsglobal.org/spec/lti-dl/claim/data'] = idtoken.platformContext.deepLinkingSettings.data
-
-      provDeepLinkingDebug('Sanitizing content item array based on the platform\'s requirements:')
-      const selectedContentItems = []
-
-      const acceptedTypes = idtoken.platformContext.deepLinkingSettings.accept_types
-      const acceptMultiple = !(idtoken.platformContext.deepLinkingSettings.accept_multiple === 'false' || idtoken.platformContext.deepLinkingSettings.accept_multiple === false)
-
-      provDeepLinkingDebug('Accepted Types: ' + acceptedTypes)
-      provDeepLinkingDebug('Accepts Mutiple: ' + acceptMultiple)
-
-      provDeepLinkingDebug('Received content items: ')
-      provDeepLinkingDebug(contentItems)
-
-      for (const contentItem of contentItems) {
-        if (!acceptedTypes.includes(contentItem.type)) continue
-        selectedContentItems.push(contentItem)
-        if (!acceptMultiple) break
-      }
-      provDeepLinkingDebug('Content items to be sent: ')
-      provDeepLinkingDebug(selectedContentItems)
-      jwtBody['https://purl.imsglobal.org/spec/lti-dl/claim/content_items'] = selectedContentItems
-
-      const message = jwt.sign(jwtBody, await platform.platformPrivateKey(), { algorithm: 'RS256', keyid: await platform.platformKid() })
-      return message
-    } catch (err) {
-      provDeepLinkingDebug(err.message)
-      if (this.#logger) this.#logger.log({ level: 'error', message: 'Message: ' + err.message + '\nStack: ' + err.stack })
-      return false
+    // Gets platform
+    const platform = await this.#getPlatform(idtoken.iss, this.#ENCRYPTIONKEY, this.#logger, this.#Database)
+    if (!platform) {
+      provDeepLinkingDebug('Platform not found')
+      throw new Error('PLATFORM_NOT_FOUND')
     }
+
+    provDeepLinkingDebug('Building basic JWT body')
+    // Builds basic jwt body
+    const jwtBody = {
+      iss: await platform.platformClientId(),
+      aud: idtoken.iss,
+      iat: Date.now() / 1000,
+      exp: Date.now() / 1000 + 60,
+      nonce: crypto.randomBytes(16).toString('base64'),
+      'https://purl.imsglobal.org/spec/lti/claim/deployment_id': idtoken.deploymentId,
+      'https://purl.imsglobal.org/spec/lti/claim/message_type': 'LtiDeepLinkingResponse',
+      'https://purl.imsglobal.org/spec/lti/claim/version': '1.3.0'
+    }
+
+    // Adding messaging options
+    if (options.message) jwtBody['https://purl.imsglobal.org/spec/lti-dl/claim/msg'] = options.message
+    if (options.errmessage) jwtBody['https://purl.imsglobal.org/spec/lti-dl/claim/errormsg '] = options.errmessage
+    if (options.log) jwtBody['https://purl.imsglobal.org/spec/lti-dl/claim/log'] = options.log
+    if (options.errlog) jwtBody['https://purl.imsglobal.org/spec/lti-dl/claim/errorlog'] = options.errlog
+
+    // Adding Data claim if it exists in initial request
+    if (idtoken.platformContext.deepLinkingSettings.data) jwtBody['https://purl.imsglobal.org/spec/lti-dl/claim/data'] = idtoken.platformContext.deepLinkingSettings.data
+
+    provDeepLinkingDebug('Sanitizing content item array based on the platform\'s requirements:')
+    const selectedContentItems = []
+
+    const acceptedTypes = idtoken.platformContext.deepLinkingSettings.accept_types
+    const acceptMultiple = !(idtoken.platformContext.deepLinkingSettings.accept_multiple === 'false' || idtoken.platformContext.deepLinkingSettings.accept_multiple === false)
+
+    provDeepLinkingDebug('Accepted Types: ' + acceptedTypes)
+    provDeepLinkingDebug('Accepts Mutiple: ' + acceptMultiple)
+
+    provDeepLinkingDebug('Received content items: ')
+    provDeepLinkingDebug(contentItems)
+
+    for (const contentItem of contentItems) {
+      if (!acceptedTypes.includes(contentItem.type)) continue
+      selectedContentItems.push(contentItem)
+      if (!acceptMultiple) break
+    }
+    provDeepLinkingDebug('Content items to be sent: ')
+    provDeepLinkingDebug(selectedContentItems)
+    jwtBody['https://purl.imsglobal.org/spec/lti-dl/claim/content_items'] = selectedContentItems
+
+    const message = jwt.sign(jwtBody, await platform.platformPrivateKey(), { algorithm: 'RS256', keyid: await platform.platformKid() })
+    return message
   }
 }
 
