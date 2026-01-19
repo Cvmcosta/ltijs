671d28263f6ff6f7dbaa14387b154d5931fb0c88
Marco <maidh91@gmail.com>
2026-01-19 15:50:56 +1100

    revert req.cookies.lti_token

--- src/Utils/Server.js
+++ src/Utils/Server.js
@@ -94,10 +94,6 @@ class Server {
           return next()
         }
       }
-      if (req.cookies.lti_token) {
-        req.token = req.cookies.lti_token;
-        return next();
-      }
       return next()
     })
 
